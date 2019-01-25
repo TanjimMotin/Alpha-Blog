07a3065b8047f7122f6ff807ba2ce830aedf98bd
Tanjim Motin <tanjimotin21@gmail.com>
2019-01-24 22:12:30 -0500

    show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,11 +6,26 @@ end
 def create
 	@article = Article.new(article_params)
 
-    @article.save
+    if @article.save
 
-    redirect_to article_path(@article)
-	
+    flash[:notice] = "Article was successfully created"
+
+   redirect_to article_path(@article)
+
+   else
+
+   render 'new'
+
+  end
 end
+def show
+
+    @article = Article.find(params[:id])
+
+end
+
+
+
 
   private
 
